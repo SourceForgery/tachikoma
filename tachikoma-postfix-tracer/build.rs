576833b41fb03afcc6b34454f355c25d3ea59491
Tobias <tobias@olsson.be>
2017-12-27 18:42:39 +0100

    Working grpc

--- tachikoma-postfix-tracer/build.rs
+++ tachikoma-postfix-tracer/build.rs
@@ -1,10 +1,12 @@
 extern crate protoc_rust_grpc;
 
 fn main() {
+
+
     protoc_rust_grpc::run(protoc_rust_grpc::Args {
         out_dir: "src",
-        includes: &["proto"],
-        input: &["proto/aaa.proto", "proto/bbb.proto"],
+        includes: &["build/api/"],
+        input: &["build/api/test.proto"],
         rust_protobuf: true, // also generate protobuf messages, not just services
     }).expect("protoc-rust-grpc");
 }
