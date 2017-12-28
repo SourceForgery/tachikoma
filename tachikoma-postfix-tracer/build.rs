7b7859d4b80fb653bc689ce5234de652ea182f92
Tobias <tobias@olsson.be>
2017-12-28 08:37:35 +0100

    Create output directory

--- tachikoma-postfix-tracer/build.rs
+++ tachikoma-postfix-tracer/build.rs
@@ -1,16 +1,17 @@
 extern crate protoc_rust_grpc;
 extern crate glob;
 
-use glob::glob;
 use std::path::PathBuf;
+use std::fs;
 
-const DIR: &'static str = "build/grpc-api/";
+const INPUT_DIR: &'static str = "build/grpc-api/";
+const OUTPUT_DIR: &'static str = "src/generated-grpc/";
 
 
 fn main() {
     let mut list: Vec<String> = Vec::new();
-    let glob_pattern = String::from(DIR) + "/**/*.proto";
-    for entry in glob(glob_pattern.as_ref()).expect(format!("{} is not found.", DIR).as_ref()) {
+    let glob_pattern = String::from(INPUT_DIR) + "/**/*.proto";
+    for entry in glob::glob(glob_pattern.as_ref()).expect(format!("{} is not found.", INPUT_DIR).as_ref()) {
         if let Ok(path) = entry {
             let pth: PathBuf = path;
             let pth2 = pth.to_str().expect("Encoding error in filename?").to_owned();
@@ -19,9 +20,11 @@ fn main() {
     }
     let v2: Vec<&str> = list.iter().map(|s| &**s).collect();
 
+    fs::create_dir_all(OUTPUT_DIR).expect(format!("Could not create directory {}", OUTPUT_DIR).as_ref());
+
     protoc_rust_grpc::run(protoc_rust_grpc::Args {
-        out_dir: "src/generated-grpc",
-        includes: &[DIR],
+        out_dir: OUTPUT_DIR,
+        includes: &[INPUT_DIR],
         input: v2.as_slice(),
         rust_protobuf: true, // also generate protobuf messages, not just services
     }).expect("protoc-rust-grpc");
