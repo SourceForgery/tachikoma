e77cf8bcb0562d1a27ca75a0877e9c09be846db8
Tobias <tobias@olsson.be>
2018-01-02 19:40:34 +0100

    Hook up Binders

--- tachikoma-grpc/src/main/kotlin/com/sourceforgery/tachikoma/GrpcBinder.kt
+++ tachikoma-grpc/src/main/kotlin/com/sourceforgery/tachikoma/GrpcBinder.kt
@@ -6,14 +6,18 @@ import com.sourceforgery.tachikoma.tracking.TrackingDecoder
 import com.sourceforgery.tachikoma.tracking.TrackingDecoderImpl
 import io.grpc.BindableService
 import org.glassfish.hk2.utilities.binding.AbstractBinder
+import javax.inject.Singleton
 
 class GrpcBinder : AbstractBinder() {
     override fun configure() {
         bind(MTADeliveryService::class.java)
                 .to(BindableService::class.java)
+                .`in`(Singleton::class.java)
         bind(MTAEmailQueueService::class.java)
                 .to(BindableService::class.java)
+                .`in`(Singleton::class.java)
         bind(TrackingDecoderImpl::class.java)
                 .to(TrackingDecoder::class.java)
+                .`in`(Singleton::class.java)
     }
 }
--- tachikoma-rest/src/main/kotlin/com/sourceforgery/rest/RestBinder.kt
+++ tachikoma-rest/src/main/kotlin/com/sourceforgery/rest/RestBinder.kt
@@ -2,10 +2,12 @@ package com.sourceforgery.rest
 
 import com.sourceforgery.rest.tracking.TrackingRest
 import org.glassfish.hk2.utilities.binding.AbstractBinder
+import javax.inject.Singleton
 
 class RestBinder : AbstractBinder() {
     override fun configure() {
         bind(TrackingRest::class.java)
                 .to(RestService::class.java)
+                .`in`(Singleton::class.java)
     }
 }
