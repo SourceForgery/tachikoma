c84d1d434d8a02dcdcc106fd25db273f429ae5ad
Tobias <tobias@olsson.be>
2018-01-02 11:35:08 +0100

    Add mq config

--- tachikoma-startup/src/main/kotlin/com/sourceforgery/tachikoma/config/Configuration.kt
+++ tachikoma-startup/src/main/kotlin/com/sourceforgery/tachikoma/config/Configuration.kt
@@ -1,10 +1,12 @@
 package com.sourceforgery.tachikoma.config
 
+import com.sourceforgery.tachikoma.mq.MqConfig
 import com.sourceforgery.tachikoma.tracking.TrackingConfig
 import java.net.URI
 
-internal class Configuration : DatabaseConfig, TrackingConfig {
+internal class Configuration : DatabaseConfig, TrackingConfig, MqConfig {
     override val sqlUrl = readConfig("SQL_URL", "postgres://username:password@localhost:5432/tachikoma", URI::class.java)
     override val createDatabase = readConfig("CREATE_DATABASE", true)
     override val trackingEncryptionKey: String = readConfig("TRACKING_ENCRYPTION_KEY", "peilieK6RoomaiPhainocool6ebezai0ox7qui4p")
+    override val mqUrl: URI = readConfig("MQ_URL", "amqp://guest:guest@localhost/tachikoma", URI::class.java);
 }
