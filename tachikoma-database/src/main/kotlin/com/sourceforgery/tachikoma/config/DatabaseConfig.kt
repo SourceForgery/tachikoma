1fec90f5a851aa011a68b4104096ab6667d977c3
Tobias <tobias@olsson.be>
2018-01-06 10:22:56 +0100

    Sort config variables

--- tachikoma-database/src/main/kotlin/com/sourceforgery/tachikoma/config/DatabaseConfig.kt
+++ tachikoma-database/src/main/kotlin/com/sourceforgery/tachikoma/config/DatabaseConfig.kt
@@ -3,8 +3,8 @@ package com.sourceforgery.tachikoma.config
 import java.net.URI
 
 interface DatabaseConfig {
-    val sqlUrl: URI
     val createDatabase: Boolean
+    val sqlUrl: URI
     val timeDatabaseQueries: Boolean
     val wipeAndCreateDatabase: Boolean
 }
