57ce537c71ce8ee6a2dacb48fc20c09def14a17c
Tobias <tobias@olsson.be>
2018-01-06 11:36:42 +0100

    Add another table for allowing/disallowing sending

---
+++ tachikoma-database/src/main/kotlin/com/sourceforgery/tachikoma/database/objects/AllowedSendingEmailDBO.kt
@@ -0,0 +1,19 @@
+package com.sourceforgery.tachikoma.database.objects
+
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.OneToMany
+import javax.persistence.Table
+
+@Table(name = "e_allowed_sending_email")
+@Entity
+class AllowedSendingEmailDBO(
+        @Column
+        val domainPart: String,
+        @Column
+        val localPart: String? = null,
+        @OneToMany
+        val account: AccountDBO
+) : GenericDBO() {
+}
+
