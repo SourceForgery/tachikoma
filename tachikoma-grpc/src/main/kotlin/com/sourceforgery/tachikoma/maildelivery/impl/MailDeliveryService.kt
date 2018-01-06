ee06c35e75e2241f14be2970f1d49e4cc12fff4e
Tobias <tobias@olsson.be>
2018-01-06 17:07:07 +0100

    Add job factory and add job to send email to queue

--- tachikoma-grpc/src/main/kotlin/com/sourceforgery/tachikoma/maildelivery/impl/MailDeliveryService.kt
+++ tachikoma-grpc/src/main/kotlin/com/sourceforgery/tachikoma/maildelivery/impl/MailDeliveryService.kt
@@ -6,30 +6,36 @@ import com.fasterxml.jackson.databind.node.ObjectNode
 import com.github.mustachejava.DefaultMustacheFactory
 import com.google.protobuf.Struct
 import com.google.protobuf.util.JsonFormat
+import com.sourceforgery.tachikoma.common.toInstant
 import com.sourceforgery.tachikoma.database.dao.EmailDAO
 import com.sourceforgery.tachikoma.database.objects.EmailDBO
 import com.sourceforgery.tachikoma.database.objects.EmailSendTransactionDBO
 import com.sourceforgery.tachikoma.database.objects.SentMailMessageBodyDBO
 import com.sourceforgery.tachikoma.database.objects.id
 import com.sourceforgery.tachikoma.database.server.DBObjectMapper
+import com.sourceforgery.tachikoma.grpc.frontend.maildelivery.EmailQueueStatus
 import com.sourceforgery.tachikoma.grpc.frontend.maildelivery.MailDeliveryServiceGrpc
 import com.sourceforgery.tachikoma.grpc.frontend.maildelivery.OutgoingEmail
-import com.sourceforgery.tachikoma.grpc.frontend.maildelivery.EmailQueueStatus
 import com.sourceforgery.tachikoma.grpc.frontend.maildelivery.Queued
 import com.sourceforgery.tachikoma.grpc.frontend.toGrpcInternal
 import com.sourceforgery.tachikoma.grpc.frontend.toNamedEmail
+import com.sourceforgery.tachikoma.mq.JobFactory
+import com.sourceforgery.tachikoma.mq.MQSender
 import io.grpc.Status
 import io.grpc.StatusRuntimeException
 import io.grpc.stub.StreamObserver
 import java.io.StringReader
 import java.io.StringWriter
+import java.time.Instant
 import javax.inject.Inject
 
 internal class MailDeliveryService
 @Inject
 private constructor(
         private val dbObjectMapper: DBObjectMapper,
-        private val emailDAO: EmailDAO
+        private val emailDAO: EmailDAO,
+        private val mqSender: MQSender,
+        private val jobFactory: JobFactory
 ) : MailDeliveryServiceGrpc.MailDeliveryServiceImplBase() {
 
     override fun sendEmail(request: OutgoingEmail, responseObserver: StreamObserver<EmailQueueStatus>) {
@@ -50,14 +56,25 @@ private constructor(
         val mailMessageBody = SentMailMessageBodyDBO(
                 body = wrapAndPackBody(request, static.htmlBody, static.plaintextBody)
         )
+        val requestedSendTime =
+                if (request.hasSendAt()) {
+                    request.sendAt.toInstant()
+                } else {
+                    Instant.EPOCH
+                }
 
         val emailSent = request.recipientsList.map {
+            // TODO Check if recipient is blocked
 
             val emailDBO = EmailDBO(
                     recipient = it.toNamedEmail(),
                     transaction = transaction,
                     sentEmailMessageBodyDBO = mailMessageBody
             )
+            mqSender.queueJob(jobFactory.createSendEmailJob(
+                    requestedSendTime = requestedSendTime,
+                    emailId = emailDBO.id
+            ))
             emailDAO.save(emailDBO)
             responseObserver.onNext(
                     EmailQueueStatus.newBuilder()
@@ -83,9 +100,21 @@ private constructor(
                 jsonRequest = getRequestData(request)
         )
         val transactionId = transaction.id.toGrpcInternal()
+        val requestedSendTime =
+                if (request.hasSendAt()) {
+                    request.sendAt.toInstant()
+                } else {
+                    Instant.EPOCH
+                }
 
-        val globalVars: Struct = template.globalVars ?: Struct.getDefaultInstance()
+        val globalVars: Struct =
+                if (template.hasGlobalVars()) {
+                    template.globalVars
+                } else {
+                    Struct.getDefaultInstance()
+                }
         val emailSent = request.recipientsList.map {
+            // TODO Check if recipient is blocked
             val templateVars = it.templateVars ?: Struct.getDefaultInstance()
             val htmlBody = mergeTemplate(template.htmlTemplate, globalVars, templateVars)
             val plaintextBody = mergeTemplate(template.plaintextTemplate, globalVars, templateVars)
@@ -101,7 +130,12 @@ private constructor(
                     transaction = transaction,
                     sentEmailMessageBodyDBO = mailMessageBody
             )
+
             emailDAO.save(emailDBO)
+            mqSender.queueJob(jobFactory.createSendEmailJob(
+                    requestedSendTime = requestedSendTime,
+                    emailId = emailDBO.id
+            ))
 
             responseObserver.onNext(
                     EmailQueueStatus.newBuilder()
--- tachikoma-internal-api/src/main/kotlin/com/sourceforgery/tachikoma/CommonBinder.kt
+++ tachikoma-internal-api/src/main/kotlin/com/sourceforgery/tachikoma/CommonBinder.kt
@@ -1,12 +1,16 @@
 package com.sourceforgery.tachikoma
 
 import com.sourceforgery.tachikoma.common.Clocker
+import com.sourceforgery.tachikoma.mq.JobFactory
 import org.glassfish.hk2.utilities.binding.AbstractBinder
 import java.time.Clock
+import javax.inject.Singleton
 
 class CommonBinder : AbstractBinder() {
     override fun configure() {
         bind(Clocker())
                 .to(Clock::class.java)
+        bindAsContract(JobFactory::class.java)
+                .`in`(Singleton::class.java)
     }
 }
