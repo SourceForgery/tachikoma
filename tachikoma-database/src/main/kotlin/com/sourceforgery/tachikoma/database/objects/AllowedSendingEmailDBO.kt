c809f6e1e8e96d61c40db660a9297e464f208bf2
Tobias <tobias@olsson.be>
2018-01-06 13:41:47 +0100

    Remove unused import

--- tachikoma-database/src/main/kotlin/com/sourceforgery/tachikoma/database/objects/AllowedSendingEmailDBO.kt
+++ tachikoma-database/src/main/kotlin/com/sourceforgery/tachikoma/database/objects/AllowedSendingEmailDBO.kt
@@ -3,7 +3,6 @@ package com.sourceforgery.tachikoma.database.objects
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.ManyToOne
-import javax.persistence.OneToMany
 import javax.persistence.Table
 
 @Table(name = "e_allowed_sending_email")
