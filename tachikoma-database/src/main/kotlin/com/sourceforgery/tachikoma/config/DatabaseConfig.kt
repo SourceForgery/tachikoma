0b318a14d785aee262b9446329ad20ae936bbf0d
Tobias <tobias@olsson.be>
2018-01-07 01:41:29 +0100

    Remove duplicate config variable

--- tachikoma-database/src/main/kotlin/com/sourceforgery/tachikoma/config/DatabaseConfig.kt
+++ tachikoma-database/src/main/kotlin/com/sourceforgery/tachikoma/config/DatabaseConfig.kt
@@ -3,7 +3,6 @@ package com.sourceforgery.tachikoma.config
 import java.net.URI
 
 interface DatabaseConfig {
-    val createDatabase: Boolean
     val databaseEncryptionKey: String
     val sqlUrl: URI
     val timeDatabaseQueries: Boolean
