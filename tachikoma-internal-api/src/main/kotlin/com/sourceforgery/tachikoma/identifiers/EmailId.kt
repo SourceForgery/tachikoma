f42841e91ed9982c4fcd43bf01a017b994b56f98
Tobias <tobias@olsson.be>
2018-01-07 16:16:18 +0100

    Add emailId helper

--- tachikoma-internal-api/src/main/kotlin/com/sourceforgery/tachikoma/identifiers/EmailId.kt
+++ tachikoma-internal-api/src/main/kotlin/com/sourceforgery/tachikoma/identifiers/EmailId.kt
@@ -2,4 +2,8 @@ package com.sourceforgery.tachikoma.identifiers
 
 data class EmailId(val emailId: Long) {
     override fun toString() = "EmailId: emailId"
+
+    companion object {
+        fun fromList(emailIds: List<Long>) = emailIds.map { EmailId(it) }
+    }
 }
