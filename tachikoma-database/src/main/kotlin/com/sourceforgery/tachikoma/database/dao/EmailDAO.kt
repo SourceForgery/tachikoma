f4ec04f327ccf38d6117b2d53ac1520d6cbbfb70
Tobias <tobias@olsson.be>
2018-01-07 16:13:12 +0100

    Add message to find emails by id and body

--- tachikoma-database/src/main/kotlin/com/sourceforgery/tachikoma/database/dao/EmailDAO.kt
+++ tachikoma-database/src/main/kotlin/com/sourceforgery/tachikoma/database/dao/EmailDAO.kt
@@ -2,6 +2,7 @@ package com.sourceforgery.tachikoma.database.dao
 
 import com.sourceforgery.tachikoma.database.objects.EmailDBO
 import com.sourceforgery.tachikoma.identifiers.EmailId
+import com.sourceforgery.tachikoma.identifiers.SentMailMessageBodyId
 import io.ebean.EbeanServer
 import javax.inject.Inject
 
@@ -12,6 +13,13 @@ private constructor(
 ) {
     fun fetchEmailData(emailMessageId: EmailId) =
             ebeanServer.find(EmailDBO::class.java, emailMessageId.emailId)
+
+    fun fetchEmailData(emailMessageIds: List<EmailId>, sentMailMessageBodyId: SentMailMessageBodyId) =
+            ebeanServer.find(EmailDBO::class.java)
+                    .where()
+                    .eq("sentMailMessageBody.dbId", sentMailMessageBodyId.sentMailMessageBodyId)
+                    .`in`("dbId", emailMessageIds.map { it.emailId })
+                    .findList()
 
     fun save(emailDBO: EmailDBO) = ebeanServer.save(emailDBO)
 }
