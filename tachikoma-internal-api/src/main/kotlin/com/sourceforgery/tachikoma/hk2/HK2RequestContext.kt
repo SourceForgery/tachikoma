501b6f21f629fbe93465fabdbef2cc94b6f37b76
Tobias <tobias@olsson.be>
2018-01-18 23:39:46 +0100

    Fix HK2 Request context a bit

--- tachikoma-internal-api/src/main/kotlin/com/sourceforgery/tachikoma/hk2/HK2RequestContext.kt
+++ tachikoma-internal-api/src/main/kotlin/com/sourceforgery/tachikoma/hk2/HK2RequestContext.kt
@@ -4,4 +4,8 @@ import org.glassfish.hk2.api.ServiceLocator
 
 interface HK2RequestContext {
     fun <T> runInScope(task: (ServiceLocator) -> T): T
+    fun getContextInstance(): ReqCtxInstance
+    fun <T> runInScope(ctx: ReqCtxInstance, task: (ServiceLocator) -> T): T
 }
+
+interface ReqCtxInstance
