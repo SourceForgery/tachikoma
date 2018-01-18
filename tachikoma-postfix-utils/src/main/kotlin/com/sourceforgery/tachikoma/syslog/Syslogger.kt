50d8a9bf52839a0d555390bb8367f72405c89c42
Tobias <tobias@olsson.be>
2018-01-18 23:58:01 +0100

    Listen to mail syslog
    Need to collect good/bad messages, parse and send back

--- tachikoma-postfix-utils/src/main/kotlin/com/sourceforgery/tachikoma/syslog/Syslogger.kt
+++ tachikoma-postfix-utils/src/main/kotlin/com/sourceforgery/tachikoma/syslog/Syslogger.kt
@@ -3,20 +3,21 @@
 package com.sourceforgery.tachikoma.syslog
 
 import com.sourceforgery.tachikoma.logging.logger
-import java.io.BufferedReader
 import java.io.File
-import java.io.FileReader
+import java.io.RandomAccessFile
 
 class Syslogger {
     fun blockingSniffer() {
         LOGGER.info("Started logging")
         val file = File("/opt/maillog_pipe")
         try {
-            BufferedReader(FileReader(file)).use { pipe ->
-                LOGGER.info("Actually reading file")
-                while (true) {
+            while (!Thread.interrupted()) {
+                RandomAccessFile(file, "r").use { pipe ->
                     val line = pipe.readLine()!!
-                    LOGGER.error { ">>>> $line <<<<" }
+                    // >>>>Jan 18 22:55:46 1c7326acd8e5 postfix/smtp[249]: 2D61E2A03: to=<test@example.com>, relay=none, delay=30, delays=0.01/0/30/0, dsn=4.4.1, status=deferred (connect to example.com[93.184.216.34]:25: Connection timed out)<<<<
+
+
+                    LOGGER.error { ">>>>$line<<<<" }
                 }
             }
         } finally {
