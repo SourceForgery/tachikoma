8ebb77f90fd9d5051da68f36b30298a45d45220b
Tobias <tobias@olsson.be>
2018-01-18 12:36:48 +0100

    gRPC is not fond of null, so nonnull it

---
+++ tachikoma-frontend-api-proto/tachikoma-frontend-api-jvm/src/main/java/com/sourceforgery/tachikoma/grpc/frontend/auth/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@MethodsAreNonnullByDefault
+package com.sourceforgery.tachikoma.grpc.frontend.auth;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+import com.sourceforgery.tachikoma.annotations.MethodsAreNonnullByDefault;
---
+++ tachikoma-internal-proto-jvm/src/main/java/com/sourceforgery/tachikoma/mq/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@MethodsAreNonnullByDefault
+package com.sourceforgery.tachikoma.mq;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+import com.sourceforgery.tachikoma.annotations.MethodsAreNonnullByDefault;
