dc1a67613871f0193992ec398350a3b5c5ca2036
Robin Wallberg <robin.wallberg@youcruit.com>
2018-01-22 14:27:51 +0100

    Create email from local and domain part

--- tachikoma-internal-api/src/main/kotlin/com/sourceforgery/tachikoma/common/Email.kt
+++ tachikoma-internal-api/src/main/kotlin/com/sourceforgery/tachikoma/common/Email.kt
@@ -2,11 +2,22 @@ package com.sourceforgery.tachikoma.common
 
 import com.sourceforgery.tachikoma.identifiers.MailDomain
 
-class Email(
-        val address: String
-) {
-    val domain = MailDomain(address.substringAfter('@'))
-    val localPart = address.substringBefore('@')
+class Email {
+    constructor(domain: MailDomain, localPart: String) {
+        this.domain = domain
+        this.localPart = localPart
+        this.address = "$localPart@$domain"
+    }
+
+    constructor(address: String) {
+        this.address = address
+        domain = MailDomain(address.substringAfter('@'))
+        localPart = address.substringBefore('@')
+    }
+
+    val address: String
+    val domain: MailDomain
+    val localPart: String
 
     // Don't change this. Used for string templates
     override fun toString() = address
