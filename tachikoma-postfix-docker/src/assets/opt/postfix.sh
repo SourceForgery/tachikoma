5847999377569f043b41e25197b316b72737e911
Robin Wallberg <robin.wallberg@youcruit.com>
2018-01-22 09:34:40 +0100

    SASL & TLS config in docker

--- tachikoma-postfix-docker/src/assets/opt/postfix.sh
+++ tachikoma-postfix-docker/src/assets/opt/postfix.sh
@@ -3,6 +3,37 @@
 #postconf -F '*/*/chroot = n'
 postconf -e myhostname=$MAIL_DOMAIN
 
+# SASL
+postconf -e smtpd_sasl_auth_enable=yes
+postconf -e broken_sasl_auth_clients=yes
+postconf -e smtpd_recipient_restrictions=permit_sasl_authenticated,reject_unauth_destination
+# smtpd.conf
+cat >> /etc/postfix/sasl/smtpd.conf <<EOF
+pwcheck_method: auxprop
+auxprop_plugin: sasldb
+mech_list: PLAIN LOGIN CRAM-MD5 DIGEST-MD5 NTLM
+EOF
+
+# sasldb2
+echo $SMTP_USER | tr , \\n > /tmp/passwd
+while IFS=':' read -r _user _pwd; do
+  echo $_pwd | saslpasswd2 -p -c -u $MAIL_DOMAIN $_user
+done < /tmp/passwd
+chown postfix.sasl /etc/sasldb2
+
+# TLS
+if [[ -n "$(find /etc/postfix/certs -iname *.crt)" && -n "$(find /etc/postfix/certs -iname *.key)" ]]; then
+  postconf -e smtpd_tls_cert_file=$(find /etc/postfix/certs -iname *.crt)
+  postconf -e smtpd_tls_key_file=$(find /etc/postfix/certs -iname *.key)
+  chmod 400 /etc/postfix/certs/*.*
+  postconf -M submission/inet="submission   inet   n   -   n   -   -   smtpd"
+  postconf -P "submission/inet/syslog_name=postfix/submission"
+  postconf -P "submission/inet/smtpd_tls_security_level=encrypt"
+  postconf -P "submission/inet/smtpd_sasl_auth_enable=yes"
+  postconf -P "submission/inet/milter_macro_daemon_name=ORIGINATING"
+  postconf -P "submission/inet/smtpd_recipient_restrictions=permit_sasl_authenticated,reject_unauth_destination"
+fi
+
 service postfix start
 
 touch /var/log/mail.log
