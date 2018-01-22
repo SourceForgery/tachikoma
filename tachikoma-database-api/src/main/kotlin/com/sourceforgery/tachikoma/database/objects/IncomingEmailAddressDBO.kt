6ea89eaeb2a6770794ae5ede7c8035deda86428e
Robin Wallberg <robin.wallberg@youcruit.com>
2018-01-22 14:26:16 +0100

    Split email into local and domain

--- tachikoma-database-api/src/main/kotlin/com/sourceforgery/tachikoma/database/objects/IncomingEmailAddressDBO.kt
+++ tachikoma-database-api/src/main/kotlin/com/sourceforgery/tachikoma/database/objects/IncomingEmailAddressDBO.kt
@@ -1,16 +1,22 @@
 package com.sourceforgery.tachikoma.database.objects
 
-import com.sourceforgery.tachikoma.common.Email
+import com.sourceforgery.tachikoma.identifiers.MailDomain
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.ManyToOne
 import javax.persistence.Table
+import javax.persistence.UniqueConstraint
 
-@Table(name = "e_incoming_email_address")
+@Table(name = "e_incoming_email_address", uniqueConstraints = [
+    UniqueConstraint(columnNames = ["local_part", "mail_domain"])
+])
 @Entity
 class IncomingEmailAddressDBO(
-        @Column(unique = true)
-        val email: Email,
+        @Column
+        val localPart: String?,
+        @Column
+        val mailDomain: MailDomain,
+
         @ManyToOne
         val account: AccountDBO
 ) : GenericDBO()
