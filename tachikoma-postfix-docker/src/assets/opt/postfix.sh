0e1900a5f0b756d043300fec62f5be4f0ca6d9b8
Robin Wallberg <robin.wallberg@youcruit.com>
2018-01-22 14:28:39 +0100

    Remove SASL auth from postfix config

--- tachikoma-postfix-docker/src/assets/opt/postfix.sh
+++ tachikoma-postfix-docker/src/assets/opt/postfix.sh
@@ -1,25 +1,14 @@
 #!/bin/bash -eu
 
 #postconf -F '*/*/chroot = n'
-postconf -e myhostname=$MAIL_DOMAIN
+#postconf -e myhostname=$MAIL_DOMAIN
 
-# SASL
-postconf -e smtpd_sasl_auth_enable=yes
-postconf -e broken_sasl_auth_clients=yes
-postconf -e smtpd_recipient_restrictions=permit_sasl_authenticated,reject_unauth_destination
-# smtpd.conf
-cat >> /etc/postfix/sasl/smtpd.conf <<EOF
-pwcheck_method: auxprop
-auxprop_plugin: sasldb
-mech_list: PLAIN LOGIN CRAM-MD5 DIGEST-MD5 NTLM
-EOF
+postconf -e virtual_transport=lmtp:unix:private/incoming_tachikoma
+postconf -e virtual_mailbox_domains=$MAIL_DOMAIN
+postconf -e virtual_mailbox_maps=hash:/etc/postfix/vmailbox
 
-# sasldb2
-echo $SMTP_USER | tr , \\n > /tmp/passwd
-while IFS=':' read -r _user _pwd; do
-  echo $_pwd | saslpasswd2 -p -c -u $MAIL_DOMAIN $_user
-done < /tmp/passwd
-chown postfix.sasl /etc/sasldb2
+echo "@$MAIL_DOMAIN whatever" > /etc/postfix/vmailbox
+postmap hash:/etc/postfix/vmailbox
 
 # TLS
 if [[ -n "$(find /etc/postfix/certs -iname *.crt)" && -n "$(find /etc/postfix/certs -iname *.key)" ]]; then
@@ -28,10 +17,7 @@ if [[ -n "$(find /etc/postfix/certs -iname *.crt)" && -n "$(find /etc/postfix/ce
   chmod 400 /etc/postfix/certs/*.*
   postconf -M submission/inet="submission   inet   n   -   n   -   -   smtpd"
   postconf -P "submission/inet/syslog_name=postfix/submission"
-  postconf -P "submission/inet/smtpd_tls_security_level=encrypt"
-  postconf -P "submission/inet/smtpd_sasl_auth_enable=yes"
   postconf -P "submission/inet/milter_macro_daemon_name=ORIGINATING"
-  postconf -P "submission/inet/smtpd_recipient_restrictions=permit_sasl_authenticated,reject_unauth_destination"
 fi
 
 service postfix start
