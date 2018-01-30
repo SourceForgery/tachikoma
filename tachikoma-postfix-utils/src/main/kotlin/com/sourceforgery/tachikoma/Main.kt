fd5d2d3a6685c49b74232d95dd191e712b728cb0
Tobias <tobias@olsson.be>
2018-01-30 21:46:17 +0100

    Split up postfix utils starter

--- tachikoma-postfix-utils/src/main/kotlin/com/sourceforgery/tachikoma/Main.kt
+++ tachikoma-postfix-utils/src/main/kotlin/com/sourceforgery/tachikoma/Main.kt
@@ -1,6 +1,7 @@
 package com.sourceforgery.tachikoma
 
 import com.sourceforgery.tachikoma.incoming.IncomingEmail
+import com.sourceforgery.tachikoma.logging.logger
 import com.sourceforgery.tachikoma.mailer.MailSender
 import com.sourceforgery.tachikoma.syslog.Syslogger
 import io.grpc.ManagedChannelBuilder
@@ -11,6 +12,50 @@ import java.util.concurrent.TimeUnit
 
 private val APITOKEN_HEADER = Metadata.Key.of("x-apitoken", Metadata.ASCII_STRING_MARSHALLER)
 
+class Main(
+        private val tachikomaUrl: URI,
+        private val mailDomain: String
+) {
+
+    private fun withoutPassword(uri: URI): String {
+        val query = uri.rawQuery?.let { "?$it" } ?: ""
+        return "${uri.scheme}://${uri.host}${uri.path ?: "/"}$query"
+    }
+
+    fun run() {
+        LOGGER.info { "Connecting to ${withoutPassword(tachikomaUrl)}" }
+
+        val plaintext = tachikomaUrl.scheme == "http"
+        val port: Int =
+                if (tachikomaUrl.port == -1) {
+                    when (tachikomaUrl.scheme) {
+                        "http" -> 80
+                        "https" -> 443
+                        else -> throw IllegalArgumentException("Unknown proto's default port is unknown")
+                    }
+                } else {
+                    tachikomaUrl.port
+                }
+
+        val metadataAuth = Metadata()
+        metadataAuth.put(APITOKEN_HEADER, tachikomaUrl.userInfo)
+
+        val channel = ManagedChannelBuilder.forAddress(tachikomaUrl.host, port)
+                .intercept(MetadataUtils.newAttachHeadersInterceptor(metadataAuth))
+                .usePlaintext(plaintext)
+                .idleTimeout(365, TimeUnit.DAYS)
+                .build()
+        MailSender(channel).start()
+        IncomingEmail(channel).start()
+        Syslogger(channel).blockingSniffer()
+    }
+
+    companion object {
+        private val LOGGER = logger()
+    }
+}
+
+
 fun main(args: Array<String>) {
 
     val mailDomain = System.getenv("MAIL_DOMAIN")
@@ -20,27 +65,6 @@ fun main(args: Array<String>) {
             System.getenv("TACHIKOMA_URL")
                     ?: "http://$mailDomain:oodua5yai9Pah5ook3wah4hahqu4IeK0iung8ou5Cho4Doonee@172.17.0.1:8070"
     )
-    val plaintext = tachikomaUrl.scheme == "http"
-    val port: Int =
-            if (tachikomaUrl.port == -1) {
-                when (tachikomaUrl.scheme) {
-                    "http" -> 80
-                    "https" -> 443
-                    else -> throw IllegalArgumentException("Unknown proto's default port is unknown")
-                }
-            } else {
-                tachikomaUrl.port
-            }
-
-    val metadataAuth = Metadata()
-    metadataAuth.put(APITOKEN_HEADER, tachikomaUrl.userInfo)
-
-    val channel = ManagedChannelBuilder.forAddress(tachikomaUrl.host, port)
-            .intercept(MetadataUtils.newAttachHeadersInterceptor(metadataAuth))
-            .usePlaintext(plaintext)
-            .idleTimeout(365, TimeUnit.DAYS)
-            .build()
-    MailSender(channel).start()
-    IncomingEmail(channel).start()
-    Syslogger(channel).blockingSniffer()
+    Main(tachikomaUrl, mailDomain)
+            .run()
 }
