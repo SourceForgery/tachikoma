619b1a2d112a1ae1289cc35e4eae8aece79a9c90
Tobias <tobias@olsson.be>
2018-01-31 23:55:59 +0100

    Get SSL to work in postfix-utils

--- tachikoma-postfix-utils/src/main/kotlin/com/sourceforgery/tachikoma/Main.kt
+++ tachikoma-postfix-utils/src/main/kotlin/com/sourceforgery/tachikoma/Main.kt
@@ -4,47 +4,65 @@ import com.sourceforgery.tachikoma.incoming.IncomingEmail
 import com.sourceforgery.tachikoma.logging.logger
 import com.sourceforgery.tachikoma.mailer.MailSender
 import com.sourceforgery.tachikoma.syslog.Syslogger
-import io.grpc.ManagedChannelBuilder
 import io.grpc.Metadata
+import io.grpc.netty.GrpcSslContexts
+import io.grpc.netty.NettyChannelBuilder
 import io.grpc.stub.MetadataUtils
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory
+import io.netty.util.internal.logging.InternalLoggerFactory
+import io.netty.util.internal.logging.Log4J2LoggerFactory
 import java.net.URI
 import java.util.concurrent.TimeUnit
 
 private val APITOKEN_HEADER = Metadata.Key.of("x-apitoken", Metadata.ASCII_STRING_MARSHALLER)
 
 class Main(
-        private val tachikomaUrl: URI,
+        urlWithoutDomain: URI,
         private val mailDomain: String
 ) {
+
+    private val tachikomaUrl = addDomain(urlWithoutDomain)
 
     private fun withoutPassword(uri: URI): String {
         val query = uri.rawQuery?.let { "?$it" } ?: ""
         return "${uri.scheme}://${uri.host}${uri.path ?: "/"}$query"
     }
 
-    fun run() {
-        LOGGER.info { "Connecting to ${withoutPassword(tachikomaUrl)}" }
-
-        val plaintext = tachikomaUrl.scheme == "http"
+    private fun addDomain(uri: URI): URI {
+        val query = uri.rawQuery?.let { "?$it" } ?: ""
         val port: Int =
-                if (tachikomaUrl.port == -1) {
-                    when (tachikomaUrl.scheme) {
+                if (uri.port == -1) {
+                    when (uri.scheme) {
                         "http" -> 80
                         "https" -> 443
                         else -> throw IllegalArgumentException("Unknown proto's default port is unknown")
                     }
                 } else {
-                    tachikomaUrl.port
+                    uri.port
                 }
+        return URI.create("${uri.scheme}://$mailDomain:${uri.userInfo}@${uri.host}:$port${uri.path ?: "/"}$query")
+    }
+
+    fun run() {
+        LOGGER.info { "Connecting to ${withoutPassword(tachikomaUrl)}" }
+
+        val plaintext = tachikomaUrl.scheme == "http"
 
         val metadataAuth = Metadata()
         metadataAuth.put(APITOKEN_HEADER, tachikomaUrl.userInfo)
 
-        val channel = ManagedChannelBuilder.forAddress(tachikomaUrl.host, port)
+        val channel = NettyChannelBuilder.forAddress(tachikomaUrl.host, tachikomaUrl.port)
                 .intercept(MetadataUtils.newAttachHeadersInterceptor(metadataAuth))
                 .apply {
                     if (plaintext) {
                         usePlaintext(true)
+                    } else {
+                        useTransportSecurity()
+                        sslContext(
+                                GrpcSslContexts.forClient()
+                                        .trustManager(InsecureTrustManagerFactory.INSTANCE)
+                                        .build()
+                        )
                     }
                 }
                 .idleTimeout(365, TimeUnit.DAYS)
@@ -66,7 +84,8 @@ fun main(args: Array<String>) {
 
     val tachikomaUrl = URI.create(
             System.getenv("TACHIKOMA_URL")
-                    ?: "http://$mailDomain:oodua5yai9Pah5ook3wah4hahqu4IeK0iung8ou5Cho4Doonee@172.17.0.1:8070"
+                    ?: "http://oodua5yai9Pah5ook3wah4hahqu4IeK0iung8ou5Cho4Doonee@172.17.0.1:8070"
+
     )
     Main(tachikomaUrl, mailDomain)
             .run()
