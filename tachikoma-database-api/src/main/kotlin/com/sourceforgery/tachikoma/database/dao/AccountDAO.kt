7cc0c34abc06d975c6804e05218b17016c91b6fa
Tobias <tobias@olsson.be>
2018-02-07 23:19:11 +0100

    Add method for finding account by maildomain

--- tachikoma-database-api/src/main/kotlin/com/sourceforgery/tachikoma/database/dao/AccountDAO.kt
+++ tachikoma-database-api/src/main/kotlin/com/sourceforgery/tachikoma/database/dao/AccountDAO.kt
@@ -2,8 +2,10 @@ package com.sourceforgery.tachikoma.database.dao
 
 import com.sourceforgery.tachikoma.database.objects.AccountDBO
 import com.sourceforgery.tachikoma.identifiers.AccountId
+import com.sourceforgery.tachikoma.identifiers.MailDomain
 
 interface AccountDAO {
     fun save(account: AccountDBO)
     fun getById(accountId: AccountId): AccountDBO
+    fun getByMailDomain(mailDomain: MailDomain): AccountDBO?
 }
