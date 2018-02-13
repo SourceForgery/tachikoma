6c159b6c3c101537c6ca323153e2bb7464c91f06
Robin Wallberg <robin.wallberg@youcruit.com>
2018-02-13 11:10:35 +0100

    Removed condition.

--- tachikoma-database/src/main/kotlin/com/sourceforgery/tachikoma/database/upgrades/Version5.kt
+++ tachikoma-database/src/main/kotlin/com/sourceforgery/tachikoma/database/upgrades/Version5.kt
@@ -9,7 +9,7 @@ class Version5 : DatabaseUpgrade {
                 .use {
                     it.execute("ALTER TABLE e_account ADD COLUMN incoming_mx_domain VARCHAR")
                     it.execute("ALTER TABLE e_account ADD CONSTRAINT incoming_mx_domain UNIQUE (mail_domain)")
-                    it.execute("UPDATE e_account SET incoming_mx_domain = mail_domain WHERE incoming_mx_domain IS NULL")
+                    it.execute("UPDATE e_account SET incoming_mx_domain = mail_domain")
                     it.execute("ALTER TABLE e_account ALTER COLUMN incoming_mx_domain SET NOT NULL")
                 }
         return -5
