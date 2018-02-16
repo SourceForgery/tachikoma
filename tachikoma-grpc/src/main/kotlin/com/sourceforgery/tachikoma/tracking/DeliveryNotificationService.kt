6c772ee1656ecb369c774c71f0c627d4eb969d16
Tobias <tobias@olsson.be>
2018-02-16 13:54:17 +0100

    Refactor frontend delivery notifications
    Add Future that closes http/2 channel when MQ quits

--- tachikoma-grpc/src/main/kotlin/com/sourceforgery/tachikoma/tracking/DeliveryNotificationService.kt
+++ tachikoma-grpc/src/main/kotlin/com/sourceforgery/tachikoma/tracking/DeliveryNotificationService.kt
@@ -2,6 +2,7 @@ package com.sourceforgery.tachikoma.tracking
 
 import com.google.protobuf.Empty
 import com.sourceforgery.tachikoma.database.dao.EmailDAO
+import com.sourceforgery.tachikoma.database.objects.EmailDBO
 import com.sourceforgery.tachikoma.database.objects.id
 import com.sourceforgery.tachikoma.grpc.frontend.ClickedEvent
 import com.sourceforgery.tachikoma.grpc.frontend.DeliveredEvent
@@ -20,7 +21,9 @@ import com.sourceforgery.tachikoma.identifiers.EmailId
 import com.sourceforgery.tachikoma.logging.logger
 import com.sourceforgery.tachikoma.mq.DeliveryNotificationMessage
 import com.sourceforgery.tachikoma.mq.MQSequenceFactory
+import io.grpc.stub.ServerCallStreamObserver
 import io.grpc.stub.StreamObserver
+import java.util.concurrent.Executors
 import javax.inject.Inject
 
 internal class DeliveryNotificationService
@@ -34,69 +37,94 @@ private constructor(
             request: NotificationStreamParameters,
             authenticationId: AuthenticationId
     ) {
-        mqSequenceFactory.listenForDeliveryNotifications(authenticationId, {
-            val emailData = emailDAO.fetchEmailData(emailMessageId = EmailId(it.emailMessageId))
+        val serverCallStreamObserver = responseObserver as? ServerCallStreamObserver
+        val deliveryNotificationCallback = { deliveryNotificationMessage: DeliveryNotificationMessage ->
+            val emailData = emailDAO.fetchEmailData(emailMessageId = EmailId(deliveryNotificationMessage.emailMessageId))
             if (emailData == null) {
-                LOGGER.error("Got event with non-existing email " + it.emailMessageId)
+                LOGGER.error("Got event with non-existing email " + deliveryNotificationMessage.emailMessageId)
             } else {
-                val notificationBuilder = EmailNotification.newBuilder()
-                notificationBuilder.emailId = emailData.id.toGrpcInternal()
-                notificationBuilder.recipientEmailAddress = emailData.recipient.toGrpcInternal()
-                notificationBuilder.emailTransactionId = emailData.transaction.id.toGrpcInternal()
-                notificationBuilder.timestamp = it.creationTimestamp
-                notificationBuilder.messageId = MessageId.newBuilder().setMessageId(emailData.messageId.messageId).build()
-                if (request.includeTrackingData) {
-                    notificationBuilder.emailTrackingData =
-                            SentEmailTrackingData.newBuilder()
-                                    .addAllTags(emailData.transaction.tags)
-                                    .putAllMetadata(emailData.transaction.metaData)
-                                    .putAllMetadata(emailData.metaData)
-                                    .build()
-                } else {
-                    notificationBuilder.setNoTrackingData(Empty.getDefaultInstance())
-                }
-                if (request.includeSubject) {
-                    emailData.subject?.also {
-                        notificationBuilder.subject = it
-                    }
-                }
-                @Suppress("UNUSED_VARIABLE")
-                val ignored = when (it.notificationDataCase) {
-                    DeliveryNotificationMessage.NotificationDataCase.MESSAGECLICKED -> {
-                        notificationBuilder.clickedEvent = ClickedEvent.newBuilder()
-                                .setIpAddress(it.messageClicked.ipAddress)
-                                .setClickedUrl(it.messageClicked.clickedUrl)
-                                .build()
-                    }
-                    DeliveryNotificationMessage.NotificationDataCase.MESSAGEHARDBOUNCED -> {
-                        notificationBuilder.hardBouncedEvent = HardBouncedEvent.getDefaultInstance()
-                    }
-                    DeliveryNotificationMessage.NotificationDataCase.MESSAGEOPENED -> {
-                        notificationBuilder.openedEvent = OpenedEvent.newBuilder().setIpAddress(it.messageOpened.ipAddress).build()
-                    }
-                    DeliveryNotificationMessage.NotificationDataCase.MESSAGEDELIVERED -> {
-                        notificationBuilder.delivereddEvent = DeliveredEvent.getDefaultInstance()
-                    }
-                    DeliveryNotificationMessage.NotificationDataCase.MESSAGESOFTBOUNCED -> {
-                        notificationBuilder.softBouncedEvent = SoftBouncedEvent.getDefaultInstance()
-                    }
-                    DeliveryNotificationMessage.NotificationDataCase.MESSAGEUNSUBSCRIBED -> {
-                        notificationBuilder.unsubscribedEvent = UnsubscribedEvent.getDefaultInstance()
-                    }
-                    DeliveryNotificationMessage.NotificationDataCase.MESSAGEQUEUED -> {
-                        notificationBuilder.queuedEvent = QueuedEvent.getDefaultInstance()
-                    }
-                    DeliveryNotificationMessage.NotificationDataCase.NOTIFICATIONDATA_NOT_SET -> {
-                        // Skipping obviously bad message
-                        throw RuntimeException("Message without event. Just wrong.")
-                    }
-                }
-                responseObserver.onNext(notificationBuilder.build())
+                val emailNotification = deliveryNotificationMessage.toEmailNotification(emailData, request)
+                responseObserver.onNext(emailNotification)
             }
-        })
+        }
+        val future = mqSequenceFactory.listenForDeliveryNotifications(
+                authenticationId = authenticationId,
+                callback = deliveryNotificationCallback
+        )
+        future.addListener(
+                Runnable {
+                    val cancelled = serverCallStreamObserver?.isCancelled ?: true
+                    if (!cancelled) {
+                        responseObserver.onCompleted()
+                    }
+                },
+                responseCloser
+        )
     }
 
     companion object {
-        val LOGGER = logger()
+        private val LOGGER = logger()
+        private val responseCloser = Executors.newCachedThreadPool()!!
+    }
+}
+
+private fun DeliveryNotificationMessage.toEmailNotification(emailData: EmailDBO, request: NotificationStreamParameters): EmailNotification? {
+    val notificationBuilder = EmailNotification.newBuilder()
+    notificationBuilder.emailId = emailData.id.toGrpcInternal()
+    notificationBuilder.recipientEmailAddress = emailData.recipient.toGrpcInternal()
+    notificationBuilder.emailTransactionId = emailData.transaction.id.toGrpcInternal()
+    notificationBuilder.timestamp = this.creationTimestamp
+    notificationBuilder.messageId = MessageId.newBuilder().setMessageId(emailData.messageId.messageId).build()
+    if (request.includeTrackingData) {
+        notificationBuilder.emailTrackingData =
+                SentEmailTrackingData.newBuilder()
+                        .addAllTags(emailData.transaction.tags)
+                        .putAllMetadata(emailData.transaction.metaData)
+                        .putAllMetadata(emailData.metaData)
+                        .build()
+    } else {
+        notificationBuilder.setNoTrackingData(Empty.getDefaultInstance())
+    }
+    if (request.includeSubject) {
+        emailData.subject?.also {
+            notificationBuilder.subject = it
+        }
+    }
+    notificationBuilder.setEventData(this)
+    return notificationBuilder
+            .build()
+}
+
+private fun EmailNotification.Builder.setEventData(deliveryNotificationMessage: DeliveryNotificationMessage): Any {
+    @Suppress("WHEN_ENUM_CAN_BE_NULL_IN_JAVA")
+    return when (deliveryNotificationMessage.notificationDataCase) {
+        DeliveryNotificationMessage.NotificationDataCase.MESSAGECLICKED -> {
+            clickedEvent = ClickedEvent.newBuilder()
+                    .setIpAddress(deliveryNotificationMessage.messageClicked.ipAddress)
+                    .setClickedUrl(deliveryNotificationMessage.messageClicked.clickedUrl)
+                    .build()
+        }
+        DeliveryNotificationMessage.NotificationDataCase.MESSAGEHARDBOUNCED -> {
+            hardBouncedEvent = HardBouncedEvent.getDefaultInstance()
+        }
+        DeliveryNotificationMessage.NotificationDataCase.MESSAGEOPENED -> {
+            openedEvent = OpenedEvent.newBuilder().setIpAddress(deliveryNotificationMessage.messageOpened.ipAddress).build()
+        }
+        DeliveryNotificationMessage.NotificationDataCase.MESSAGEDELIVERED -> {
+            delivereddEvent = DeliveredEvent.getDefaultInstance()
+        }
+        DeliveryNotificationMessage.NotificationDataCase.MESSAGESOFTBOUNCED -> {
+            softBouncedEvent = SoftBouncedEvent.getDefaultInstance()
+        }
+        DeliveryNotificationMessage.NotificationDataCase.MESSAGEUNSUBSCRIBED -> {
+            unsubscribedEvent = UnsubscribedEvent.getDefaultInstance()
+        }
+        DeliveryNotificationMessage.NotificationDataCase.MESSAGEQUEUED -> {
+            queuedEvent = QueuedEvent.getDefaultInstance()
+        }
+        DeliveryNotificationMessage.NotificationDataCase.NOTIFICATIONDATA_NOT_SET -> {
+            // Skipping obviously bad message
+            throw RuntimeException("Message without event. Just wrong.")
+        }
     }
 }
