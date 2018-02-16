146cd8e966e7c8d0040b21f714e26e9cb10b4d6a
Tobias <tobias@olsson.be>
2018-02-16 15:50:56 +0100

    Add unsubscribe links in email

--- tachikoma-rest/src/main/kotlin/com/sourceforgery/rest/unsubscribe/UnsubscribeRest.kt
+++ tachikoma-rest/src/main/kotlin/com/sourceforgery/rest/unsubscribe/UnsubscribeRest.kt
@@ -1,13 +1,16 @@
 package com.sourceforgery.rest.unsubscribe
 
+import com.linecorp.armeria.common.HttpHeaders
 import com.linecorp.armeria.common.HttpResponse
 import com.linecorp.armeria.common.HttpStatus
+import com.linecorp.armeria.common.MediaType
 import com.linecorp.armeria.server.annotation.ConsumeType
 import com.linecorp.armeria.server.annotation.ConsumeTypes
 import com.linecorp.armeria.server.annotation.Get
 import com.linecorp.armeria.server.annotation.Param
 import com.linecorp.armeria.server.annotation.Post
 import com.sourceforgery.rest.RestService
+import com.sourceforgery.rest.tracking.TrackingRest
 import com.sourceforgery.tachikoma.common.EmailStatus
 import com.sourceforgery.tachikoma.common.toTimestamp
 import com.sourceforgery.tachikoma.database.dao.BlockedEmailDAO
@@ -24,6 +27,8 @@ import com.sourceforgery.tachikoma.mq.MQSender
 import com.sourceforgery.tachikoma.mq.MessageUnsubscribed
 import com.sourceforgery.tachikoma.tracking.RemoteIP
 import com.sourceforgery.tachikoma.unsubscribe.UnsubscribeDecoder
+import io.netty.util.AsciiString
+import java.text.MessageFormat
 import java.time.Clock
 import javax.inject.Inject
 
@@ -65,16 +70,27 @@ private constructor(
         try {
             val unsubscribeData = createAndSendUnsubscribeEvent(unsubscribeDataString)
             val redirectUrl = unsubscribeData.redirectUrl
+            if (redirectUrl.isBlank()) {
+                return HttpResponse.of(HttpStatus.OK)
+            } else {
+                return HttpResponse.of(
+                        HttpStatus.TEMPORARY_REDIRECT,
+                        MediaType.HTML_UTF_8,
+                        TrackingRest.HTML_PAGE_WITH_JAVASCRIPT_AND_HTTP_EQUIV_REDIRECT.format(arrayOf(redirectUrl)),
+                        HttpHeaders.of(TrackingRest.LOCATION, redirectUrl)
+                )
+            }
         } catch (e: Exception) {
             LOGGER.warn { "Failed to unsubscribe $unsubscribeDataString with error ${e.message}" }
             LOGGER.debug(e, { "Failed to unsubscribe $unsubscribeDataString" })
+            return HttpResponse.of(HttpStatus.OK)
         }
-        return HttpResponse.of(HttpStatus.OK)
     }
 
     private fun createAndSendUnsubscribeEvent(unsubscribeDataString: String): UnsubscribeData {
         val unsubscribeData = unsubscribeDecoder.decodeUnsubscribeData(unsubscribeDataString)
         val email = emailDAO.fetchEmailData(unsubscribeData.emailId.toEmailId())!!
+        LOGGER.info { "Received unsubscribe event from ${email.recipient} for email ${email.id}" }
         val emailStatusEvent = EmailStatusEventDBO(
                 emailStatus = EmailStatus.UNSUBSCRIBE,
                 email = email,
@@ -101,6 +117,18 @@ private constructor(
 
     companion object {
         val ONE_CLICK_FORM_DATA = "One-Click"
+        val LOCATION = AsciiString.of("Location")!!
+        val HTML_PAGE_WITH_JAVASCRIPT_AND_HTTP_EQUIV_REDIRECT = MessageFormat("""
+            <html>
+              <head>
+                <meta http-equiv="refresh" content="0;URL=''{0}''" />
+              </head>
+              <body>
+                <script type="text/javascript">document.location.href=''{0}'';</script>
+                <a href="{0}">redirect</a>
+              </body>
+            </html>
+            """.trimMargin())
         val LOGGER = logger()
     }
 }
