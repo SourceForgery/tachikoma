cf256c52b515c04932c95fdd472e32e85f63fb72
Robin Wallberg <robin.wallberg@youcruit.com>
2018-02-27 11:21:18 +0100

    Cleanup logging.

--- tachikoma-grpc/src/test/kotlin/com/sourceforgery/tachikoma/H2DataSourceProvider.kt
+++ tachikoma-grpc/src/test/kotlin/com/sourceforgery/tachikoma/H2DataSourceProvider.kt
@@ -1,6 +1,5 @@
 package com.sourceforgery.tachikoma
 
-import com.sourceforgery.tachikoma.config.DatabaseConfig
 import com.sourceforgery.tachikoma.database.server.DataSourceProvider
 import io.ebean.config.ServerConfig
 import io.ebean.config.dbplatform.h2.H2Platform
@@ -12,7 +11,6 @@ import javax.inject.Inject
 class H2DataSourceProvider
 @Inject
 private constructor(
-        private val databaseConfig: DatabaseConfig
 ) : DataSourceProvider {
     override fun provide(serverConfig: ServerConfig) {
         val dataSourceConfig = DataSourceConfig()
@@ -24,8 +22,6 @@ private constructor(
         dataSourceConfig.url = "jdbc:h2:mem:tests-${UUID.randomUUID()};DB_CLOSE_DELAY=-1"
         dataSourceConfig.username = "sa"
         dataSourceConfig.password = "blank"
-
-        System.err.println("DataSourceProvider.provide ${dataSourceConfig.url}, databaseConfig: ${databaseConfig.sqlUrl}")
 
         serverConfig.dataSourceConfig = dataSourceConfig
         serverConfig.databasePlatform = H2Platform()
