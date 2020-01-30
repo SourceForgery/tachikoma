624e208f280ee48fe1ba4326c7298b1dd66d25eb
Snōwball <tobias@olsson.be>
2020-01-30 14:43:04 +0100

    Squelch warning from gradle-docker by upgrading plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -15,7 +15,7 @@ dependencies {
     implementation("org.eclipse.jgit:org.eclipse.jgit:5.3.0.201903130848-r")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     implementation("org.junit.platform:junit-platform-gradle-plugin:1.2.0")
-    implementation("se.transmode.gradle:gradle-docker:1.2-youcruit-8")
+    implementation("se.transmode.gradle:gradle-docker:1.2-youcruit-9")
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:0.10.0")
     implementation("com.github.ben-manes:gradle-versions-plugin:0.17.0")
 }
