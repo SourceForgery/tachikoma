13307ff81e0e39c2ed94ce374bfef59ee080a970
Patrick Gilmore <p@youcruit.com>
2020-02-20 15:33:40 +0100

    Remove trim

--- tachikoma-grpc/src/main/kotlin/com/sourceforgery/tachikoma/unsubscribe/UnsubscribeDecoderImpl.kt
+++ tachikoma-grpc/src/main/kotlin/com/sourceforgery/tachikoma/unsubscribe/UnsubscribeDecoderImpl.kt
@@ -37,6 +37,6 @@ private constructor(
             .setMessage(ByteString.copyFrom(parcelled))
             .setSignature(ByteString.copyFrom(signature))
             .build()
-        return Base64.getUrlEncoder().encodeToString(signedMessage.toByteArray()).trimEnd('=')
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(signedMessage.toByteArray())
     }
 }
