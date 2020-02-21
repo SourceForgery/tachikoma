e0f478c566a85ff1fe05ecb9d1a93b475e1bd342
Snōwball <tobias@olsson.be>
2020-02-21 11:40:16 +0100

    Fix opendkim startup

--- tachikoma-postfix-docker/src/assets/opt/opendkim.sh
+++ tachikoma-postfix-docker/src/assets/opt/opendkim.sh
@@ -25,7 +25,7 @@ SignatureAlgorithm      rsa-sha256
 UserID                  opendkim:opendkim
 Socket                  inet:8891@localhost
 SignHeaders             From,Reply-To,Sender,To,CC,Subject,Message-Id,Date,List-Unsubscribe-Post,List-Unsubscribe,X-Tachikoma-User
-AlwaysSignHeaders       From,Reply-To,Sender,To,CC,Subject,Message-Id,Date,List-Unsubscribe-Post,List-Unsubscribe,X-Tachikoma-User
+# AlwaysSignHeaders       From,Reply-To,Sender,To,CC,Subject,Message-Id,Date,List-Unsubscribe-Post,List-Unsubscribe,X-Tachikoma-User
 EOF
 
 echo -n >/etc/opendkim/KeyTable
