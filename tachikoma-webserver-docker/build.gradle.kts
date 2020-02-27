d23708be4547b76e3a1ffe7349c947f487bf2f36
Patrick Gilmore <p@youcruit.com>
2020-02-27 11:45:16 +0100

    Added support for google cloud sql proxy

--- tachikoma-webserver-docker/build.gradle.kts
+++ tachikoma-webserver-docker/build.gradle.kts
@@ -13,20 +13,22 @@ val webserverDocker by tasks.registering(se.transmode.gradle.plugins.docker.Dock
 
     setEnvironment("DEBIAN_FRONTEND", "noninteractive")
 
-    runCommand("apt-get update && apt-get -y --no-install-recommends install rsyslog rsyslog-gnutls less nvi openjdk-11-jdk-headless && apt-get clean && rm -rf /var/lib/apt/lists/*")
+    runCommand("apt-get update && apt-get -y --no-install-recommends install curl rsyslog rsyslog-gnutls less nvi openjdk-11-jdk-headless && apt-get clean && rm -rf /var/lib/apt/lists/*")
 
     runCommand("""
         mkdir -p /etc/tachikoma/rsyslog/ &&
         chown -R syslog:syslog /etc/tachikoma/rsyslog/ &&
         touch /etc/tachikoma/rsyslog/external.conf &&
-        ln -s /etc/tachikoma/rsyslog/external.conf /etc/rsyslog.d/external.conf
+        ln -s /etc/tachikoma/rsyslog/external.conf /etc/rsyslog.d/external.conf &&
+        curl -Ss https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 -o /usr/bin/cloud_sql_proxy &&
+        chmod 0755 /usr/bin/cloud_sql_proxy
         """.trimIndent().replace('\n', ' ')
     )
 
     exposePort(8443)
     exposePort(8070)
 
-    defaultCommand(listOf("bash", "-c", "if [ ! -f /etc/tachikoma/rsyslog/external.conf ]; then rm -f /etc/rsyslog.d/external.conf; fi ; /usr/sbin/rsyslogd && /opt/tachikoma-webserver/bin/tachikoma-webserver"))
+    defaultCommand(listOf("/start.sh"))
 
     runCommand("""
         sed -r "/(KLogPermitNonKernelFacility|imklog)/d" -i /etc/rsyslog.conf &&
---
+++ tachikoma-webserver-docker/src/assets/start.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -eu
+
+if [ ! -f /etc/tachikoma/rsyslog/external.conf ]
+then
+  rm -f /etc/rsyslog.d/external.conf
+fi
+/usr/sbin/rsyslogd
+
+# Make sure rsyslog is started first
+while [ ! -e /dev/log ]
+do
+  sleep 0.1
+done
+
+configDir=/etc/tachikoma/config
+
+if [ -r $configDir/cloudsql_credentials.json ]
+then
+  if ! [ -r $configDir/cloudsql_instances ]
+  then
+    logger -s "No $configDir/cloudsql_instances"
+    exit 1
+  fi
+
+  while /usr/bin/cloud_sql_proxy \
+   -verbose=false \
+   -credential_file=$configDir/cloudsql_credentials.json \
+   -instances="$(cat $configDir/cloudsql_instances)" | logger -s || true
+  do
+    sleep 10
+  done &
+fi
+
+/opt/tachikoma-webserver/bin/tachikoma-webserver
