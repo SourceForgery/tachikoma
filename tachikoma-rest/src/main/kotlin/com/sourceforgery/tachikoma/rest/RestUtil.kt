1268f4f9d322d3c0a044ba956296ad1fb42b39b6
Patrick Gilmore <p@youcruit.com>
2020-03-03 09:38:52 +0100

    Fixed armeria redirect

--- tachikoma-rest/src/main/kotlin/com/sourceforgery/tachikoma/rest/RestUtil.kt
+++ tachikoma-rest/src/main/kotlin/com/sourceforgery/tachikoma/rest/RestUtil.kt
@@ -1,38 +1,14 @@
 package com.sourceforgery.tachikoma.rest
 
-import com.linecorp.armeria.common.HttpData
-import com.linecorp.armeria.common.HttpHeaders
+import com.linecorp.armeria.common.HttpHeaderNames
 import com.linecorp.armeria.common.HttpResponse
 import com.linecorp.armeria.common.HttpStatus
-import com.linecorp.armeria.common.MediaType
-import io.netty.util.AsciiString
-import java.nio.charset.StandardCharsets
-import java.text.MessageFormat
+import com.linecorp.armeria.common.ResponseHeaders
 
 object RestUtil {
-
-    private val HTML_PAGE_WITH_JAVASCRIPT_AND_HTTP_EQUIV_REDIRECT = MessageFormat("""
-            <html>
-              <head>
-                <meta http-equiv="refresh" content="0;URL=''{0}''" />
-              </head>
-              <body>
-                <script type="text/javascript">document.location.href=''{0}'';</script>
-                <a href="{0}">redirect</a>
-              </body>
-            </html>
-            """.trimMargin())
-    private val LOCATION = AsciiString.of("Location")!!
-
     fun httpRedirect(redirectUrl: String): HttpResponse {
         return HttpResponse.of(
-            HttpStatus.TEMPORARY_REDIRECT,
-            MediaType.HTML_UTF_8,
-            HttpData.of(
-                StandardCharsets.UTF_8,
-                HTML_PAGE_WITH_JAVASCRIPT_AND_HTTP_EQUIV_REDIRECT.format(arrayOf(redirectUrl))
-            ),
-            HttpHeaders.of(LOCATION, redirectUrl)
+            ResponseHeaders.of(HttpStatus.TEMPORARY_REDIRECT, HttpHeaderNames.LOCATION, redirectUrl)
         )
     }
 }
