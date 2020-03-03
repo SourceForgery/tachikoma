55b613ba02620da909579592e88064afd903b4d9
Patrick Gilmore <p@youcruit.com>
2020-03-03 09:11:04 +0100

    Revert redirect as Armeria does not send the location header

--- tachikoma-rest/src/main/kotlin/com/sourceforgery/tachikoma/rest/RestUtil.kt
+++ tachikoma-rest/src/main/kotlin/com/sourceforgery/tachikoma/rest/RestUtil.kt
@@ -11,9 +11,13 @@ import java.text.MessageFormat
 
 object RestUtil {
 
-    private val HTML_PAGE_WITH_REDIRECT_LINK = MessageFormat("""
+    private val HTML_PAGE_WITH_JAVASCRIPT_AND_HTTP_EQUIV_REDIRECT = MessageFormat("""
             <html>
+              <head>
+                <meta http-equiv="refresh" content="0;URL=''{0}''" />
+              </head>
               <body>
+                <script type="text/javascript">document.location.href=''{0}'';</script>
                 <a href="{0}">redirect</a>
               </body>
             </html>
@@ -26,7 +30,7 @@ object RestUtil {
             MediaType.HTML_UTF_8,
             HttpData.of(
                 StandardCharsets.UTF_8,
-                HTML_PAGE_WITH_REDIRECT_LINK.format(arrayOf(redirectUrl))
+                HTML_PAGE_WITH_JAVASCRIPT_AND_HTTP_EQUIV_REDIRECT.format(arrayOf(redirectUrl))
             ),
             HttpHeaders.of(LOCATION, redirectUrl)
         )
