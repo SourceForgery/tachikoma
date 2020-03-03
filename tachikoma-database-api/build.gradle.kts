8f06f0cc36fbeefd4335fcf277b318e5c1c526e2
Tobias <tobias@olsson.be>
2020-03-03 21:12:18 +0100

    Add and comment out query beans
    They are still not really usable until they can handle custom scalar
    types

--- tachikoma-database-api/build.gradle.kts
+++ tachikoma-database-api/build.gradle.kts
@@ -1,14 +1,17 @@
 import io.ebean.gradle.EnhancePluginExtension
 applyKotlin()
 apply(plugin = "io.ebean")
+// apply(plugin = "kotlin-kapt")
 
 dependencies {
     implementation(project(":tachikoma-internal-api"))
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
 
+//    add("kapt", "io.ebean:kotlin-querybean-generator:$querybeanVersion")
+
     implementation("io.ebean:ebean:$ebeanVersion")
-    implementation("io.ebean:ebean-querybean:$querybeanVersion")
+//    implementation("io.ebean:ebean-querybean:$querybeanVersion")
     implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
 }
 
