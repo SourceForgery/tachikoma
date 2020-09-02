305e7fbcf3f3697e5dab0153bcac97f2f06e40ab
Tobias <tobias@olsson.be>
2020-09-02 17:43:16 +0200

    Better error handling

--- tachikoma-internal-api/src/main/kotlin/com/sourceforgery/tachikoma/common/ExtractEmailMetadata.kt
+++ tachikoma-internal-api/src/main/kotlin/com/sourceforgery/tachikoma/common/ExtractEmailMetadata.kt
@@ -7,22 +7,41 @@ import javax.mail.Message
 import javax.mail.Session
 import javax.mail.internet.InternetAddress
 import javax.mail.internet.MimeMessage
+import org.apache.logging.log4j.kotlin.logger
 import org.kodein.di.DI
 import org.kodein.di.DIAware
 
 class ExtractEmailMetadata(override val di: DI) : DIAware {
     fun extract(message: ByteArray): EmailMetadata {
         val mimeMessage = MimeMessage(Session.getDefaultInstance(Properties()), ByteArrayInputStream(message))
-        return EmailMetadata(
-            from = mimeMessage.from.parseNamedEmails(),
-            replyTo = mimeMessage.replyTo.parseNamedEmails(),
-            to = mimeMessage.getRecipients(Message.RecipientType.TO).parseNamedEmails(),
-            cc = mimeMessage.getRecipients(Message.RecipientType.CC).parseNamedEmails()
-        )
+        return try {
+            EmailMetadata(
+                from = mimeMessage.from
+                    ?.parseNamedEmails()
+                    ?: emptyList(),
+                replyTo = mimeMessage.replyTo
+                    ?.parseNamedEmails()
+                    ?: emptyList(),
+                to = mimeMessage.getRecipients(Message.RecipientType.TO)
+                    ?.parseNamedEmails()
+                    ?: emptyList(),
+                cc = mimeMessage.getRecipients(Message.RecipientType.CC)
+                    ?.parseNamedEmails()
+                    ?: emptyList()
+            )
+        } catch (e: Exception) {
+            LOGGER.warn(e) { "Could not parse emails in ${mimeMessage.messageID}" }
+            EmailMetadata(listOf(), listOf(), listOf())
+        }
+    }
+
+    companion object {
+        private val LOGGER = logger()
     }
 }
 
-data class EmailMetadata(
+data class EmailMetadata
+internal constructor(
     val from: List<NamedEmail>,
     val replyTo: List<NamedEmail>,
     val to: List<NamedEmail>,
