c3ce3f9af531f86228314bb32f28f7a931c45f6f
Tobias <tobias@olsson.be>
2020-09-04 11:41:48 +0200

    Add testclient

---
+++ tachikoma-test-client/src/main/kotlin/StreamIncomingEmails.kt
@@ -0,0 +1,43 @@
+import com.google.protobuf.util.JsonFormat
+import com.linecorp.armeria.client.Clients
+import com.sourceforgery.jersey.uribuilder.ensureGproto
+import com.sourceforgery.jersey.uribuilder.withoutPassword
+import com.sourceforgery.tachikoma.grpc.frontend.maildelivery.IncomingEmailParameters
+import com.sourceforgery.tachikoma.grpc.frontend.maildelivery.MailDeliveryServiceGrpcKt
+import java.net.URI
+import java.time.Duration
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.runBlocking
+
+fun main(args: Array<String>) {
+    val frontendUri = URI.create(
+        System.getenv("TACHI_FRONTEND_URI")
+            ?: error("Need to specify env TACHI_FRONTEND_URI")
+    )
+
+    val apiToken = frontendUri.userInfo
+    val stub = Clients.builder(frontendUri.withoutPassword().ensureGproto())
+        .addHeader("x-apitoken", apiToken)
+        .responseTimeout(Duration.ofDays(365))
+        .writeTimeout(Duration.ofDays(365))
+        .build(MailDeliveryServiceGrpcKt.MailDeliveryServiceCoroutineStub::class.java)
+
+    try {
+        runBlocking {
+            stub.streamIncomingEmails(IncomingEmailParameters
+                .newBuilder()
+                .setIncludeMessageAttachments(true)
+                .setIncludeMessageHeader(true)
+                .setIncludeMessageParsedBodies(true)
+                .setIncludeMessageWholeEnvelope(true)
+                .build()
+            ).collect {
+                    System.err.println("Got email: " + JsonFormat.printer().print(it))
+                }
+            System.err.println("On complete called!")
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+    System.err.println("Done complete")
+}
