34ccc1dd86da0e469297052395325f9473e0e3a1
Tobias <tobias@olsson.be>
2021-01-18 09:15:54 +0100

    Use own dispatcher
    This is a stab in the dark to fix clients not reconnecting, it's
    stops using the default dispatcher for tasks.

--- tachikoma-postfix-utils/src/main/kotlin/com/sourceforgery/tachikoma/postfix/Main.kt
+++ tachikoma-postfix-utils/src/main/kotlin/com/sourceforgery/tachikoma/postfix/Main.kt
@@ -12,11 +12,14 @@ import com.sourceforgery.tachikoma.syslog.SyslogSniffer
 import io.netty.util.internal.logging.InternalLoggerFactory
 import io.netty.util.internal.logging.Log4J2LoggerFactory
 import java.time.Duration
+import java.util.concurrent.SynchronousQueue
+import java.util.concurrent.ThreadPoolExecutor
+import java.util.concurrent.TimeUnit
 import kotlin.system.exitProcess
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.asCoroutineDispatcher
 import org.apache.logging.log4j.Level
 import org.apache.logging.log4j.io.IoBuilder
 import org.apache.logging.log4j.kotlin.logger
@@ -32,7 +35,13 @@ internal constructor(
         LOGGER.error(exception) { "Coroutine uncaught exception" }
     }
 
-    private val scope = CoroutineScope(Dispatchers.Default + SupervisorJob() + coroutineExceptionHandler)
+    private val threadPool = ThreadPoolExecutor(
+        0, 10,
+        60L, TimeUnit.SECONDS,
+        SynchronousQueue()
+    )
+
+    private val scope = CoroutineScope(threadPool.asCoroutineDispatcher() + SupervisorJob() + coroutineExceptionHandler)
 
     fun run() {
         LOGGER.info { "Connecting to ${tachikomaUrl.withoutPassword()}" }
